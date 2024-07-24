07768f6bb97d488a481583b694589782d0524f16
Davut Dagtekin <dagtekindavut07@gmail.com>
2024-07-24 13:17:44 +0300

    Finished the Project

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const playerInput = document.getElementById("tile-number-input");
 const playerInputBtn = document.getElementById("change-number-btn");
 const paintBoard = document.getElementById("paint-board");
+const colorsContainer = document.getElementById("colors");
 
 let currentNumberOfTiles = 5;
+let currentColor = "Black";
+let canPaint = true;
+
+function displayColorOptions() {
+    const colorsOptions = ["Red", "Green" ,"Blue", "Yellow", "Violet", "Orange", "Cyan", "Gray", "" ,"Brown", "Purple", "Chartreuse", "Cornsilk", "Black", "White"]
+    for (let i = 0; i < 15;i++) {
+        const colorTile = document.createElement("div");
+        colorsContainer.appendChild(colorTile);
+        colorTile.classList.add("paint-tile");
+        colorTile.style.width = 100 + "px";
+        colorTile.style.height = 100 + "px";
+        colorTile.style.backgroundColor = colorsOptions[i];
+        colorTile.addEventListener("click", ()=>currentColor = colorsOptions[i]);
+    }
+}
 
 function displayTiles() {
     paintBoard.innerHTML = ``;
@@ -12,6 +28,7 @@ function displayTiles() {
         newTile.classList.add("color-tile");
         newTile.style.width = (500 / currentNumberOfTiles) + "px";
         newTile.style.height = (500 / currentNumberOfTiles) + "px";
+        newTile.addEventListener("mouseover", ()=>newTile.style.backgroundColor = currentColor);
     }
 }
 
@@ -29,5 +46,17 @@ function setTiles(numberOfTiles) {
 }
 
 
-
+displayColorOptions();
 playerInputBtn.addEventListener("click", ()=>setTiles(playerInput.value));
+addEventListener("keydown", (key)=>{
+    if (!key.ctrlKey) {return;}
+    if (canPaint) {
+        paintBoard.style.pointerEvents = "none";
+        canPaint = false;
+    } else {
+        paintBoard.style.pointerEvents = "all";
+        canPaint = true;
+    }
+    
+    
+});
